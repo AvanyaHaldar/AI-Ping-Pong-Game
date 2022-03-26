39658465e0fd851095ede87528d740b2c7a236c8
Avanya Haldar <82445699+AvanyaHaldar@users.noreply.github.com>
2022-03-26 12:05:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,39 @@
+rightWrist_x = 0;
+rightWrist_y = 0;
+scorerightWrist = 0;
+
 function preload() {
     
 }
 
 function setup() {
-    canvas = createCanvas(500, 450);
+    canvas = createCanvas(550, 448);
     canvas.center();
     canvas.parent("canvas");
     video = createCapture(VIDEO);
     video.hide();   
     poseNet=ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',gotPoses);
 }
 
 
 function draw() {
-    image(video, 0, 0, 500, 450);
+    image(video, 0, 0, 550, 448);
 }
 
 function modelLoaded() {
     console.log("Model Is Loaded");
+}
+
+function gotPoses(result) {
+	if (result.length > 0) {
+		rightWrist_x = results[0].pose.rightWrist.x;
+        rightWrist_y = results[0].pose.rightWrist.y;
+        console.log("rightWrist_x = " + rightWrist_x );
+		console.log(result);
+	}
+}
+
+function gotResult() {
+    
 }
